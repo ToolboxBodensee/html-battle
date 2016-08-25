6902fd7e3eae357a5972773156e45cae251b6237
Michael Malura <maluramichael@googlemail.com>
2016-08-25 21:13:01 +0200

    Add client and respond with an id on connection

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 /**
  * Created by maluramichael on 25/08/16.
  */
-
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const socket = require('socket.io');
 const io = socket(server);
+const _ = require('lodash');
 
+const uuid = require('./lib/uuid');
+
+// MIDDLEWARE -------------------------------
 app.use(express.static(__dirname + '/public'));
 
+// DATA -------------------------------
+var clients = {};
+
 // GENERAL -------------------------------
-io.on('connection', ()=> {
+io.on('connection', (socket)=> {
     console.log('connected');
+
+    var id = null;
+    for (var tries = 5; tries > 0; tries--) {
+        id = uuid();
+        if (clients[id]) {
+            continue;
+        }
+        clients[id] = {socket, points: 0};
+    }
+
+    socket.emit('receiveId', {id});
 });
 
 // CLIENT -------------------------------
-io.on('client.upload', ()=> {
+io.on('client.upload', (id)=> {
     console.log('client.upload');
 });
 
@@ -33,6 +50,6 @@ io.on('admin.clearPoints', ()=> {
     console.log('admin.clearPoints');
 });
 
-// ADMIN -------------------------------
 
+// RUN -------------------------------
 server.listen(8080);
