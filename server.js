69215f5280e4c69f535dcd329408cc28c5b50c66
Michael Malura <maluramichael@googlemail.com>
2016-08-25 21:06:04 +0200

    Add some socket io commands

--- server.js
+++ server.js
@@ -10,8 +10,29 @@ const io = socket(server);
 
 app.use(express.static(__dirname + '/public'));
 
+// GENERAL -------------------------------
 io.on('connection', ()=> {
     console.log('connected');
 });
+
+// CLIENT -------------------------------
+io.on('client.upload', ()=> {
+    console.log('client.upload');
+});
+
+// ADMIN -------------------------------
+io.on('admin.setChallenge', ()=> {
+    console.log('admin.setChallenge');
+});
+
+io.on('admin.addPoints', (user, poinds)=> {
+    console.log('admin.addPoints');
+});
+
+io.on('admin.clearPoints', ()=> {
+    console.log('admin.clearPoints');
+});
+
+// ADMIN -------------------------------
 
 server.listen(8080);
