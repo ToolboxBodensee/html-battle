c901780e2a795e566d17db5f873031dc5f979eea
Jochen Klein <klein.jochen@gmail.com>
2016-10-02 16:19:17 +0200

    fix: missing dependencies

--- public/beamerApp.js
+++ public/beamerApp.js
@@ -6,6 +6,7 @@ var modulesToLoad = [
     , 'LocalStorageModule'           //
     , 'ngIdle'                       //
     , 'pascalprecht.translate'       //
+    , 'ngSanitize'
 ];
 
 var app = angular.module('toolboxHtmlBattleBeamer', modulesToLoad).config(function($sceDelegateProvider) {
